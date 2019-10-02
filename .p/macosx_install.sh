ace7ffd2bcd7c21b5977bbefdda830b5998ddbb5
Francesco Burelli <francesco.burelli@gmail.com>
2019-10-02 21:56:34 +0200

    new completion engine

--- .p/macosx_install.sh
+++ .p/macosx_install.sh
@@ -5,6 +5,12 @@ set -x
 brew install fd
 brew install ripgrep
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+cd ~/.vim/bundle/YouCompleteMe
+python install.sh --ts-completer
+
+
 # add aliases to .zshrc
 
 # add env variabiles to env file .zshenv
