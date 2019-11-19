5b4b39041f9d76a00f4d82cff237ff682517acea
Francesco Burelli <francesco.burelli@mia-platform.eu>
2019-11-19 15:41:02 +0100

    zsh suggestions

--- macosx_brew.sh
+++ macosx_brew.sh
@@ -8,3 +8,5 @@ brew install macvim
 brew install cmake
 
 brew install zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
