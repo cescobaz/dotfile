496b2e599e68bc115efcb7fdbf2186e3cabc5536
Francesco Burelli <francesco.burelli@gmail.com>
2019-11-22 00:16:20 +0100

    install links improved

--- macosx_install_links.sh
+++ macosx_install_links.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
-SCRIPT_HOME='.'
-HOME='~'
+make_link() {
+  LINK_DIR=$(realpath $(dirname $2))
+  mkdir -p $LINK_DIR
+  ln -s $1 $2
+}
 
-ln -s "$SCRIPT_HOME/.zshrc" "$HOME/.zshrc"
-ln -s "$SCRIPT_HOME/.p" "$HOME/"
-ln -s "$SCRIPT_HOME/.vim/vimrc" "$HOME/.vim/vimrc"
-ln -s "$SCRIPT_HOME/.iterm2/com.googlecode.iterm2.plist" "$HOME/iterm2/"
+SCRIPT_HOME=$(realpath $(dirname $0))
+DHOME="$HOME"
+
+LINKS=(
+  "$SCRIPT_HOME/.zshrc:$DHOME/.zshrc"
+  "$SCRIPT_HOME/.p:$DHOME/.p"
+  "$SCRIPT_HOME/.vim/vimrc:$DHOME/.vim/vimrc"
+  "$SCRIPT_HOME/.iterm2/com.googlecode.iterm2.plist:$DHOME/.iterm2/com.googlecode.iterm2.plist"
+)
+
+for LINK in "${LINKS[@]}"
+do
+  SLINK="${LINK##*:}"
+  REAL_FILE="${LINK%%:*}"
+  echo "[INFO] installing link \"${SLINK}\" that points to \"${REAL_FILE}\""
+  if [ ! -e "$SLINK" ] && [ ! -L "$SLINK" ]
+  then 
+   make_link "$REAL_FILE" "$SLINK"
+   continue
+  fi
+  while true; do
+    echo "File at ${SLINK} exists, do you want to override it? (y/n)"
+    read RESPONSE
+    if [ $RESPONSE == "y" ]; then
+      rm -r "$SLINK"
+      make_link "$REAL_FILE" "$SLINK"
+      break
+    elif [ $RESPONSE == "n" ]; then
+      break
+    else
+      "Answer "y" or "n" please"
+    fi
+  done
+done
