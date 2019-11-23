ad0a37ad9d3746795ccf69a0190a7764440317d2
Francesco Burelli <francesco.burelli@gmail.com>
2019-11-23 19:41:31 +0100

    tmux prefix changed to q and iterm2 link updated

--- macosx_install_links.sh
+++ macosx_install_links.sh
@@ -14,7 +14,7 @@ LINKS=(
   "$SCRIPT_HOME/.p:$DHOME/.p"
   "$SCRIPT_HOME/.vim/vimrc:$DHOME/.vim/vimrc"
   "$SCRIPT_HOME/.tmux.conf:$DHOME/.tmux.conf"
-  "$SCRIPT_HOME/.iterm2/com.googlecode.iterm2.plist:$DHOME/.iterm2/com.googlecode.iterm2.plist"
+  "$SCRIPT_HOME/.iterm2:$DHOME/.iterm2"
 )
 
 for LINK in "${LINKS[@]}"
