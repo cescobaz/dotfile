a7f2c2f2a81ba834a6e699e1029f2e2a1ace8c69
Francesco Burelli <francesco.burelli@gmail.com>
2019-12-01 18:39:22 +0100

    iterm2 config change and haskell tools better installation

--- macosx_install_haskell_tools.sh
+++ macosx_install_haskell_tools.sh
@@ -6,8 +6,10 @@ echo "[INFO] installing haskell linter: hindent"
 stack install hindent
 
 echo "[INFO] installing haskell-ide-engine"
-git clone https://github.com/haskell/haskell-ide-engine --recurse-submodules
-cd haskell-ide-engine
+HASKELL_IDE_ENGINE_DIR="$HOME/.haskell-ide-engine"
+git clone https://github.com/haskell/haskell-ide-engine --recurse-submodules "$HASKELL_IDE_ENGINE_DIR"
+cd "$HASKELL_IDE_ENGINE_DIR"
 stack ./install.hs build
+echo "[INFO] haskell-ide-engine installed into $HASKELL_IDE_ENGINE_DIR, do not remove folder"
 
 cd $REF
