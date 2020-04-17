e7b112204dc8e595eb534544b5d9b79552e4348d
Francesco Burelli <francesco.burelli@gmail.com>
2020-04-17 22:30:09 +0200

    install haskell scripts now use default cabal commands

--- macosx_install_haskell_tools.sh
+++ macosx_install_haskell_tools.sh
@@ -9,14 +9,14 @@ ghcup install 8.6.5
 ghcup set 8.6.5
 
 echo "[INFO] installing haskell fixer: brittany"
-cabal v2-install floskell
+cabal install floskell
 
 echo "[INFO] installing haskell-ide-engine"
 HASKELL_IDE_ENGINE_DIR="$HOME/.haskell-ide-engine"
 git clone https://github.com/haskell/haskell-ide-engine --recurse-submodules
 cd haskell-ide-engine
-cabal v2-run ./install.hs --project-file install/shake.project hie-8.6.5
-cabal v2-run ./install.hs --project-file install/shake.project data
+cabal run ./install.hs --project-file install/shake.project hie-8.6.5
+cabal run ./install.hs --project-file install/shake.project data
 cd ..
 mv haskell-ide-engine "$HASKELL_IDE_ENGINE_DIR"
 echo "[INFO] haskell-ide-engine installed into $HASKELL_IDE_ENGINE_DIR, do not remove folder"
