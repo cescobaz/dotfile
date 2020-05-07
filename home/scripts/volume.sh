5d093cd3d2f5a7c00044996c2156197a8553a8ac
Francesco Burelli <francesco.burelli@protonmail.com>
2020-05-07 21:24:35 +0200

    lemonbar volume added

--- home/scripts/volume.sh
+++ home/scripts/volume.sh
@@ -6,7 +6,21 @@ VALUE=$2
 INDEX=$(pactl list sinks short | awk '{print $1}')
 
 get_volume() {
-  pactl list sinks | grep 'Volume' | grep -iv 'base volume' | sed 's/.* \([0-9]\+\)%.* \([0-9]\+\)%.*/\1 \2/g'
+  VOLUMES=$(pactl list sinks | grep 'Volume' | grep -iv 'base volume' | sed 's/.* \([0-9]\+\)%.* \([0-9]\+\)%.*/\1 \2/g')
+  LEFT=$(echo "$VOLUMES" | awk '{print $1}')
+  RIGHT=$(echo "$VOLUMES" | awk '{print $2}')
+  echo $((($LEFT + $RIGHT) / 2))
+}
+
+subscribe_volume() {
+  while read -r line; do
+    case $line in
+    *change*sink*)
+      INDEX=$(echo "$line" | sed 's/.*#\([0-9]\+\)$/\1/')
+      get_volume
+      ;;
+    esac
+  done
 }
 
 if [ "$ACTION" == "set" ]; then
@@ -16,4 +30,6 @@ elif [ "$ACTION" == "toggle-mute" ]; then
   pactl set-sink-mute "$INDEX" toggle
 elif [ "$ACTION" == "get-volume" ]; then
   get_volume
+elif [ "$ACTION" == "subscribe-volume" ]; then
+  pactl subscribe | subscribe_volume
 fi
