84facebe2465828841399993fb159cb3fdc8d2f0
Francesco Burelli <francesco.burelli@protonmail.com>
2020-05-10 01:00:25 +0200

    cpu script fix

--- home/scripts/cpu.sh
+++ home/scripts/cpu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cpu() {
-  vmstat $1 | awk 'NR>2{print 100 - $(NF-2)"%"}'
+  vmstat --one-header $1 100 | awk 'NR>2{print 100 - $(NF-2)"%"}'
 }
 
 if [ "$1" == 'subscribe' ]; then
   while true; do
     cpu 3
+    sleep 3
   done
 else
   cpu
