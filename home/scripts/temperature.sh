be5674e2bd0ef3e3187fd1c3cd4ce31dee8a94a5
Francesco Burelli <francesco.burelli@protonmail.com>
2020-05-23 19:37:59 +0200

    max temperature script implemented

---
+++ home/scripts/temperature.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+max_temperature() {
+  TEMPERATURES=$(sensors | grep -E '[0-9]+.*C' | sed 's/.*+\([0-9]\+\.[0-9]\+\)°C .*/\1/')
+
+  MAXTEMP=0
+  for TEMP in $TEMPERATURES; do
+    if [ $(echo "$TEMP>$MAXTEMP" | bc) == "1" ]; then
+      MAXTEMP=$TEMP
+    fi
+  done
+
+  echo $MAXTEMP
+}
+
+if [ "$1" == 'subscribe' ]; then
+  while true; do
+    echo $(max_temperature)
+    sleep 3
+  done
+else
+  max_temperature
+fi
