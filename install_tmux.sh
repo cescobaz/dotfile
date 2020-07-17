47bbdd6ebd873707122a23f04cc33df8bcab83bf
Francesco Burelli <francesco.burelli@mia-platform.eu>
2020-07-17 11:09:51 +0200

    shell_env includes base16

--- install_tmux.sh
+++ install_tmux.sh
@@ -3,18 +3,29 @@
 REF=$(realpath $(dirname $0))
 source "$REF/lib.sh"
 
-brew install tmux
-if [ "$?" != 0 ]; then
-  sudo pacman -S tmux
-fi
-if [ "$?" != 0 ]; then
-  echo "unable to install tmux via package manager"
-  exit 1
-fi
+install_bins() {
+  brew install tmux
+  if [ "$?" != 0 ]; then
+    sudo pacman -S tmux
+  fi
+  if [ "$?" != 0 ]; then
+    echo "unable to install tmux via package manager"
+    exit 1
+  fi
+}
 
-LINKS=(
-  ".tmux.conf"
-  ".config/base16-tmux.conf"
-)
+install_links() {
+  LINKS=(
+    ".tmux.conf"
+    ".config/base16-tmux.conf"
+  )
 
-create_home_links "$LINKS"
+  create_home_links "$LINKS"
+}
+
+if [ "$1" == "--only-links" ]; then
+  install_links
+else
+  install_bins
+  install_links
+fi
