bfb9be87bb1eae51b8be733a9497c2015bc22888
Francesco Burelli <francesco.burelli@gmail.com>
2020-12-07 20:25:56 +0100

    macosx-remove-ghc.sh added

---
+++ macosx-remove-ghc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf /Users/cescobaz/.ghc
+rm -rf /Users/cescobaz/.ghc-mod
+rm -rf /Users/cescobaz/.ghc/ghci_history
+rm -rf /Users/cescobaz/Library/Haskell/ghc-8.6.5-x86_64
+rm -rf /Users/cescobaz/Library/Haskell/share/doc/x86_64-osx-ghc-8.2.1
+rm -rf /Users/cescobaz/Library/Haskell/share/doc/x86_64-osx-ghc-8.6.5
+rm -rf /Users/cescobaz/Library/Haskell/share/doc/x86_64-osx-ghc-8.8.1
+rm -rf /Users/cescobaz/Library/Haskell/share/ghc-8.2.1-x86_64
+rm -rf /Users/cescobaz/Library/Haskell/share/ghc-8.6.5-x86_64
+rm -rf /Users/cescobaz/Library/Haskell/share/ghc-8.8.1-x86_64
+rm -rf /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Aliases/ghc@8.8
+rm -rf /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/ghc.rb
+rm -rf /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/ghc@8.6.rb
