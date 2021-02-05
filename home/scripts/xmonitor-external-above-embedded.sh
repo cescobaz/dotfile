4b944b2b2e949ec9bd609c6a40a8bb10f643104a
Francesco Burelli <francesco.burelli@protonmail.com>
2021-02-05 18:55:44 +0100

    linux wm fixes

--- home/scripts/xmonitor-external-above-embedded.sh
+++ home/scripts/xmonitor-external-above-embedded.sh
@@ -6,5 +6,5 @@ xrandr --output LVDS-0 --auto --primary --output DP-0 --auto --above LVDS-0
 
 bspc monitor LVDS-0 --reset-desktops I II III IV V
 bspc monitor DP-0 --reset-desktops VI VII VIII IX X
-bspc vm -r
+bspc wm --reorder-monitors LVDS-0 DP-0
 bspc wm --adopt-orphans
