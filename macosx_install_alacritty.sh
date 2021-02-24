455d22d6f5f1f1f3a9daf7f76db5040cb26e7aff
Francesco Burelli <francesco.burelli@mia-platform.eu>
2021-02-24 12:50:57 +0100

    alacritty install script semplified

--- macosx_install_alacritty.sh
+++ macosx_install_alacritty.sh
@@ -5,20 +5,10 @@ set -e
 REF=$(realpath $(dirname $0))
 source "$REF/lib.sh"
 
-brew cask list alacritty || brew cask install alacritty
-clone_or_pull https://github.com/alacritty/alacritty.git ./alacritty
+install_via_pkg_mng alacritty
 
-echo "[INFO] installing manual page"
-sudo mkdir -p /usr/local/share/man/man1
-gzip -c alacritty/extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+LINKS=(
+  ".config/alacritty"
+)
 
-echo "[INFO] installing zsh completions"
-cp alacritty/extra/completions/_alacritty ~/.zsh_functions/_alacritty
-
-echo "[INFO] installing Terminfo"
-sudo tic -xe alacritty,alacritty-direct alacritty/extra/alacritty.info
-
-echo "[INFO] cleanup"
-rm -rf ./alacritty
-
-cd "$REF"
+create_home_links "$LINKS"
