8e19260bb0abdbea562575a90d318d6bd58d3094
Francesco Burelli <francesco.burelli@gmail.com>
2021-04-03 01:06:08 +0200

    install_c_dev_tools_mac improved and c.vim file added

--- install_c_dev_tools_mac.sh
+++ install_c_dev_tools_mac.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-set -e
+REF=$(realpath $(dirname $0))
+source "$REF/lib.sh"
 
-sudo port install ccls-clang-11
+install_bins() {
+  sudo port install ccls-clang-11
 
-ln -s /opt/local/bin/clang-mp-11 /Users/cescobaz/.local/bin/clang
-ln -s /opt/local/bin/clang-format-mp-11 /Users/cescobaz/.local/bin/clang-format
+  ln -s /opt/local/bin/clang-mp-11 /Users/cescobaz/.local/bin/clang
+  ln -s /opt/local/bin/clang-format-mp-11 /Users/cescobaz/.local/bin/clang-format
+}
+
+install_links() {
+  LINKS=(
+    ".vim/ftplugin/c.vim"
+  )
+
+  create_home_links "$LINKS"
+}
+
+if [ "$1" == "--only-links" ]; then
+  install_links
+else
+  install_bins
+  install_links
+fi
