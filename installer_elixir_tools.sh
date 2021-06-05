033876e85df943770b5e02a3f271e57dfdfc3bcc
Francesco Burelli <francesco.burelli@gmail.com>
2021-06-05 12:25:50 +0200

    elixir dev tools installation fixes

---
+++ installer_elixir_tools.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REF=$(realpath $(dirname $0))
+source "$REF/lib.sh"
+
+install_bins() {
+  DEST=$HOME/.elixir-ls
+  clone_or_pull https://github.com/JakeBecker/elixir-ls.git "$DEST"
+  cd "$DEST"
+  mix deps.get
+  mix compile
+  mix elixir_ls.release
+  cd "$REF"
+}
+install_links() {
+  LINKS=(
+    ".vim/ftplugin/elixir.vim"
+  )
+  create_home_links "$LINKS"
+}
