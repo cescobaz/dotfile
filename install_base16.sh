aa2209ebd094781594d4705faaabd0569106f9d6
Francesco Burelli <francesco.burelli@gmail.com>
2022-08-15 01:22:30 +0100

    install_base16 script created

---
+++ install_base16.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+REF=$(realpath $(dirname $0))
+source "$REF/lib.sh"
+
+export BASE16_SHELL="$HOME/.config/base16-shell"
+
+clone_or_pull https://github.com/chriskempson/base16-shell.git $BASE16_SHELL
+
+cat install_base16.rc >> $HOME/.zshrc
