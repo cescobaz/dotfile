e69434af90514d978570ae3ae6ee8322ca7049b4
Francesco Burelli <francesco.burelli@gmail.com>
2022-08-15 01:45:15 +0100

    install_base16 write for bashrc also

--- install_base16.sh
+++ install_base16.sh
@@ -10,3 +10,4 @@ export BASE16_SHELL="$HOME/.config/base16-shell"
 clone_or_pull https://github.com/chriskempson/base16-shell.git $BASE16_SHELL
 
 cat $REF/install_base16.rc >> $HOME/.zshrc
+cat $REF/install_base16.rc >> $HOME/.bashrc
