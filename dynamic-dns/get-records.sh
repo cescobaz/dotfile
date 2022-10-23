5bf8b9ce3e7d5d5c621d3f8b19f7939b487a77bd
Francesco Burelli <francesco.burelli@protonmail.com>
2022-10-24 00:30:35 +0200

    remove dynamic-dns scripts (moved to dw-setup repo)

--- dynamic-dns/get-records.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-DIGITALOCEAN_TOKEN=$1
-
-curl --fail -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
-  -H "Content-Type: application/json" \
-  "https://api.digitalocean.com/v2/domains/burelli.xyz/records"
