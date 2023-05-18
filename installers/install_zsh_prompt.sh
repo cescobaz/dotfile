bcce3ceae228378df3471bc2c16395f1e919942a
Francesco Burelli <francesco.burelli@protonmail.com>
2023-05-18 14:25:44 +0200

    fix

--- installers/install_zsh_prompt.sh
+++ installers/install_zsh_prompt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo """# prompt style
+echo """
+# prompt style
 # http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
 PROMPT='%F{yellow}%~ %(1j.%j.)
 %F{yellow}> %b%f'
-RPROMPT="%(?.%F{yellow}[%?]%f.%B%F{red}[%?]%f%b"
+RPROMPT="%\(?.%F{yellow}[%?]%f.%B%F{red}[%?]%f%b"
 """ >> ~/.zshrc
