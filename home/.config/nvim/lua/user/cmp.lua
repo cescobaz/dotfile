aeb43518a2364b13458a4dfa250ca1de8c55fecb
Francesco Burelli <francesco.burelli@proton.me>
2023-06-27 15:54:54 +0200

    nvim cmp buffer from all open buffers

--- home/.config/nvim/lua/user/cmp.lua
+++ home/.config/nvim/lua/user/cmp.lua
@@ -24,7 +24,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   }, {
-    { name = 'buffer' },
+    { name = 'buffer', option = { get_bufnrs = function() return vim.api.nvim_list_bufs() end } },
     { name = 'path' },
   })
 })
