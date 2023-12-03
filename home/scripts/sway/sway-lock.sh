a00a32c5f153d3a8de979f95b9e91ce91549346b
Francesco Burelli <francesco.burelli@protonmail.com>
2023-12-03 15:40:26 +0100

    add bg image to sway-lock

--- home/scripts/sway/sway-lock.sh
+++ home/scripts/sway/sway-lock.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swaylock --daemonize -f -c 555555 -k --indicator-idle-visible -t
+swaylock --daemonize -f -c 555555 -k --indicator-idle-visible -t --image ~/wallpaper/sand.png --scaling fill
