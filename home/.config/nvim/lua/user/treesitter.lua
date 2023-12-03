c971894b972e9f7fd65ad0b5c8702c049ede6a06
Francesco Burelli <francesco.burelli@protonmail.com>
2023-12-03 23:19:57 +0100

    fix treesitter setup

--- home/.config/nvim/lua/user/treesitter.lua
+++ home/.config/nvim/lua/user/treesitter.lua
@@ -1,5 +1,9 @@
 require("nvim-treesitter.configs").setup({
   ensure_installed = { "bash", "javascript", "c", "elixir", "heex", "lua", "vim", "vimdoc", "html" },
+  sync_install = false,
+  auto_install = false,
+  ignore_install = {},
+  modules = {},
   highlight = { enable = true },
   additional_vcc_regex_highlighting = false,
   incremental_selection = {
