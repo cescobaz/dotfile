a1b58dcb7ec9693ddd43739a5020da11773322e1
Francesco Burelli <francesco.burelli@protonmail.com>
2024-03-07 17:12:19 +0100

    fix zram-enable

--- home/scripts/linux/zram-enable.sh
+++ home/scripts/linux/zram-enable.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 modprobe zram
+sleep 2
 zramctl /dev/zram0 --algorithm zstd --size 16G
-mkswap -U clear --label zram0 /dev/zram0
+mkswap --label zram0 /dev/zram0
 swapon --priority 100 /dev/zram0
