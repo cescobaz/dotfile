c80a5ed9990a079f3aa884b7e1e8f839a9e42e5f
Francesco Burelli <francesco.burelli@protonmail.com>
2024-10-02 10:34:51 +0200

    feat: add git pull --rebase --autostash in git installer

--- installers/set-git-config.sh
+++ installers/set-git-config.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 git config --global alias.gr 'log --graph --full-history --all --color --tags --decorate --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s %x1b[33m%aN <%ae>%x1b[0m (%aI)"'
+
+git config --global pull.rebase true
+git config --global rebase.autostash true
