431474fe341ae9822df0c2480e059e40e478b4d1
Francesco Burelli <francesco.burelli@protonmail.com>
2025-04-09 14:45:24 +0200

    feat: kitty-andromedai.sh start postgresql automatically

--- home/scripts/kitty-andromedai.sh
+++ home/scripts/kitty-andromedai.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-set -e
+set -ex
+
+if type "podman" >/dev/null; then
+  container_runner=podman
+else
+  container_runner=docker
+fi
+
+$container_runner start postgresql
+
 kitty --session andromedai
