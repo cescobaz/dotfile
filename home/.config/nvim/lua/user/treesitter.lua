529267299c69a5d51350ce4fbfda80153f370e82
Francesco Burelli <francesco.burelli@proton.me>
2026-04-13 10:31:47 +0200

    update treesitter config

--- home/.config/nvim/lua/user/treesitter.lua
+++ home/.config/nvim/lua/user/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter").setup({})
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'elixir', 'lua' },
+  pattern = { 'elixir', 'heex', 'lua', 'typescript', 'javascript', 'json' },
   callback = function()
     vim.treesitter.start()
   end,
